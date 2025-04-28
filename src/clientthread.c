ad5c1ae726b2f2679b0df2824f2b2ceaef5573f9
Artjöm Urban <artjoemurban@gmail.com>
2025-04-28 12:30:55 +0200

    anfang

--- src/clientthread.c
+++ src/clientthread.c
@@ -2,6 +2,13 @@
 #include "user.h"
 #include "util.h"
 
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
 void *clientthread(void *arg)
 {
 	User *self = (User *)arg;
@@ -9,6 +16,12 @@ void *clientthread(void *arg)
 	debugPrint("Client thread started.");
 
 	//TODO: Receive messages and send them to all users, skip self
+
+	char buffer[BUFFER_SIZE];
+	for(;;)
+	{
+		memset(buffer, 0, sizeof(buffer));
+	}
 
 	debugPrint("Client thread stopping.");
 	free(arg);

4a53e6162f21002838503746a328d933fda67551
Artjöm Urban <artjoemurban@gmail.com>
2025-05-13 12:11:18 +0200

    Rechtschreibfehler verbessert

--- src/network.c
+++ src/network.c
@@ -5,29 +5,31 @@
 #include "network.h"
 #include "util.h"
 
-int networkReceive(int fd, Message *buffer)
+int networkReceive(int fd, Message *buffer)  //gibt bei erfolg 0 zurück und -1 bei fehler
 {
 	uint16_t net_len;
-	//TODO: Receive length
-	ssize_t recived = recv(fd, &net_len, sizeof(net_len), MSG_WAITALL);
-	if(recived != sizeof(net_len))
+	// Receive length
+	ssize_t received = recv(fd, &net_len, sizeof(net_len), MSG_WAITALL);  // MSG_WAITALL -> recv() wird solange warten bis alle bytes übertragen sind
+																	      // sonst könnte recv() zu wenig bytes aus dem eingangs puffer lesen 
+																	      // ausname verbindung bricht ab
+	if(received != sizeof(net_len))
 	{
-		errorPrint("length field passt nicht zum protokoll");
+		errorPrint("length field passt nicht zum protokoll ( recv returned %zd): %s", received, strerror(errno));
 		return -1;
 	}
 
-	//TODO: Convert length byte order
-	buffer->len = ntohs(net_len); // wandelt erhaltene bit order auf littleEndian um, damit system sie verarbeiten kann
+	// Convert length byte order
+	buffer->len = ntohs(net_len); // Konvertiert NetzwerkByte-Order zu Host-Byte-Order
 
-	//TODO: Validate length
+	// Validate length
 	if(buffer->len > MSG_MAX)
 	{
 		errorPrint("nachricht zu lang");
 		return -1;
 	}
-	//TODO: Receive text
-	recived = recv(fd, buffer->text,buffer->len, MSG_WAITALL);
-	if(recived != buffer->len) // prüfung ob nachricht zum längenfeld passt
+	// Receive text
+	received = recv(fd, buffer->text,buffer->len, MSG_WAITALL);  
+	if(received != buffer->len) // prüfung ob nachricht zum längenfeld passt
 	{
 		errorPrint("Nachricht hat nicht selbe laenge wie length field es vorschreibt");
 		return -1;
@@ -39,16 +41,17 @@ int networkReceive(int fd, Message *buffer)
 	return 0;
 }
 
-int networkSend(int fd, const Message *buffer)
+int networkSend(int fd, const Message *buffer)  //gibt bei erfolg 0 zurück und -1 bei fehler
 {
-	//TODO: Send complete message
+	// Send complete message
 	if(buffer->len > MSG_MAX)  // solte passen da dies schon beim empfangen der Nachricht geprüft wird
 	{
 		errorPrint("Nachricht zu lang");
 		return -1;
 	}
 
-	uint16_t msg_len = htons(buffer->len); // setzt die korrekte byte order für kommunikation über das Netzwerk 
+	// Convert length byte order
+	uint16_t msg_len = htons(buffer->len); // setzt Host-Byte-Order auf Network-Byte-Order
 
 	// -> auf big Endian da dies von TCP erwartet wird
 
@@ -57,7 +60,7 @@ int networkSend(int fd, const Message *buffer)
 		return -1;
 	}
 
-	if(send(fd, buffer->text, buffer->len, 0) != buffer->len)
+	if(send(fd, buffer->text, buffer->len, 0) != buffer->len)  // sendet nachricht
 	{
 		return -1;
 	}

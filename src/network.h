fcedc0836f4f9c642534790296e5973adf0fb503
Kai Ibach <ki-235709@hs-weingarten.de>
2025-05-21 10:20:43 +0200

    Protokoll Header implementiert

--- src/network.h
+++ src/network.h
@@ -2,6 +2,12 @@
 #define CHAT_PROTOCOL_H
 
 #include <stdint.h>
+#include <stddef.h>
+
+#define MSG_MAX 512  // Maximale Länge des Textteils gemäß RFC
+#define USERNAME_MAX 32 //inkl. nullterminiert für S2C
+#define USERNAME_RAW_MAX 31 //max. Länge von Namen ohne Null-Terminator
+
 
 /* TODO: When implementing the fully-featured network protocol (including
  * login), replace this with message structures derived from the network
@@ -27,9 +33,44 @@
  {
 	//struct Header h;
  } MessageP;
+
+ typedef struct __attribute__((packed))
+ {
+	uint8_t type;
+	uint16_t len;
+	char text[MSG_MAX];
+
+ }Client2Server;
+
+
+
+ typedef struct __attribute__((packed))
+ {
+	uint8_t type;
+	uint16_t len;
+	uint64_t timeStamp;
+	char originalSender[USERNAME_MAX];
+	char text[MSG_MAX];
+
+ }Server2Client;
  
 
+ typedef struct __attribute__((packed))
+ {
+	 uint8_t type;
+	 uint16_t len;
+	 uint64_t timestamp;
+	 char name[USERNAME_RAW_MAX];  // not null-terminated
+ } UserAdded;
 
+ typedef struct __attribute__((packed))
+{
+    uint8_t type;
+    uint16_t len;
+    uint64_t timestamp;
+    uint8_t code;  // 0: left, 1: kicked, 2: error
+    char name[USERNAME_RAW_MAX];  // not null-terminated
+} UserRemoved;
 
  /* simple-client: */
 enum { MSG_MAX = 1024 };
